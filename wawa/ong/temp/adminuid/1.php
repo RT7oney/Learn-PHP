0b7ee9cbd3d003f1963ee2c423cf5b6dcfafa171
RT7oney <297085213@qq.com>
2017-08-28 15:05:03 +0800

    新增

---
+++ wawa/ong/temp/adminuid/1.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'id' => '1',
+  'name' => 'admin',
+  'pass' => '76c43cc33ce06e51868f57ff986',
+  'diqu' => '0',
+  'epass' => '00f9d6efc1fd4a4755b1f21f194',
+  'type' => '0',
+  'yanzhengip' => '1',
+  'off' => '1',
+  'ip' => '',
+  'atime' => '0',
+);
